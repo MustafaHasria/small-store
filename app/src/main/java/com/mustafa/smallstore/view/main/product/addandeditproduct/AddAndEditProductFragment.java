ea17dfd31ad93c21e0e62bc990c38419481b57d5
Mustafa Hasria <mostafahasria0101@gmail.com>
2022-05-19 11:32:40 +0300

    * Get categories from DB in AddAndEditProductFragment screen. * Add countries json file.

--- app/src/main/java/com/mustafa/smallstore/view/main/product/addandeditproduct/AddAndEditProductFragment.java
+++ app/src/main/java/com/mustafa/smallstore/view/main/product/addandeditproduct/AddAndEditProductFragment.java
@@ -2,7 +2,6 @@ package com.mustafa.smallstore.view.main.product.addandeditproduct;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +9,13 @@ import android.widget.ArrayAdapter;
 
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.mustafa.smallstore.R;
 import com.mustafa.smallstore.databinding.FragmentAddAndEditProductBinding;
 import com.mustafa.smallstore.view.datepickerstartandend.MainForDateActivity;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class AddAndEditProductFragment extends Fragment {
 
@@ -27,29 +26,37 @@ public class AddAndEditProductFragment extends Fragment {
     //endregion
 
     //region Variable
-    ArrayList<String> categoryList;
+    ArrayList<String> nameCategoryList;
     ArrayList<String> madeInList;
     ArrayAdapter<String> categoryAdapter;
     ArrayAdapter<String> madeInAdapter;
     private static String TAG = "NumberPicker";
+    AddAndEditProductViewModel addAndEditProductViewModel;
     //endregion
 
+    //region Life cycle
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_and_edit_product, container, false);
         binding = FragmentAddAndEditProductBinding.bind(view);
+        // We init this array to be sure it's not gonna take null value.
+        nameCategoryList = new ArrayList<>();
+        addAndEditProductViewModel = new ViewModelProvider(requireActivity()).get(AddAndEditProductViewModel.class);
 
-        //for Spinner And Auto Complete (category)
-        categoryList = new ArrayList<>();
-        categoryList.add("Food");
-        categoryList.add("TShirt");
-        categoryList.add("Jeans");
-        categoryAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, categoryList);
-        binding.fragmentAddAndEditProductTextInputEditTextCategory.setAdapter(categoryAdapter);
+        addAndEditProductViewModel.categoryRepository.getAllCategories().observe(requireActivity(), categoryEntities -> {
+            if (categoryEntities != null && categoryEntities.size() != 0) {
+                for (int i = 0; i < categoryEntities.size(); i++) {
+                    nameCategoryList.add(categoryEntities.get(i).getName());
+                }
+                //For Spinner And Auto Complete (category)
+                categoryAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, nameCategoryList);
+                binding.fragmentAddAndEditProductTextInputEditTextCategory.setAdapter(categoryAdapter);
+            }
+        });
 
-        //for Spinner And Auto Complete (madeIn)
+        //For Spinner And Auto Complete (madeIn)
         madeInList = new ArrayList<>();
         madeInList.add("SYRIA");
         madeInList.add("UAE");
@@ -58,9 +65,9 @@ public class AddAndEditProductFragment extends Fragment {
         binding.fragmentAddAndEditProductTextInputEditTextMadeIn.setAdapter(madeInAdapter);
 
         //For first value to number picker (initialization)
-        binding.fragmentAddAndEditProductNumberPickerHorizontal.setValue(1);
+//        binding.fragmentAddAndEditProductNumberPickerHorizontal.setValue(1);
         //get data from number picker
-        binding.fragmentAddAndEditProductNumberPickerHorizontal.setOnValueChangedListener((com.shawnlin.numberpicker.NumberPicker.OnValueChangeListener) (picker, oldVal, newVal) -> Log.d(TAG, String.format(Locale.US, "oldVal: %d, newVal: %d", oldVal, newVal)));
+//        binding.fragmentAddAndEditProductNumberPickerHorizontal.setOnValueChangedListener((com.shawnlin.numberpicker.NumberPicker.OnValueChangeListener) (picker, oldVal, newVal) -> Log.d(TAG, String.format(Locale.US, "oldVal: %d, newVal: %d", oldVal, newVal)));
         ;
 
         binding.fragmentAddAndEditProductButtonDatePicker.setOnClickListener(view1 -> {
@@ -70,5 +77,7 @@ public class AddAndEditProductFragment extends Fragment {
         });
         return view;
     }
+
+    //endregion
 
 }
---
+++ app/src/main/java/com/mustafa/smallstore/view/main/product/addandeditproduct/AddAndEditProductViewModel.java
@@ -0,0 +1,40 @@
+package com.mustafa.smallstore.view.main.product.addandeditproduct;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.mustafa.smallstore.model.entity.CategoryEntity;
+import com.mustafa.smallstore.model.entity.ProductEntity;
+import com.mustafa.smallstore.repository.CategoryRepository;
+import com.mustafa.smallstore.repository.ProductRepository;
+
+import java.util.List;
+
+public class AddAndEditProductViewModel extends AndroidViewModel {
+
+    //region Variables
+    CategoryRepository categoryRepository;
+    ProductRepository productRepository;
+    //endregion
+
+    //region Constructor
+    public AddAndEditProductViewModel(@NonNull Application application) {
+        super(application);
+        categoryRepository = new CategoryRepository(application);
+        productRepository = new ProductRepository(application);
+    }
+    //endregion
+
+    //region Methods
+    public LiveData<List<CategoryEntity>> getAllCategories() {
+        return categoryRepository.getAllCategories();
+    }
+
+    public void insertProduct(ProductEntity productEntity) {
+        productRepository.insert(productEntity);
+    }
+    //endregion
+}
